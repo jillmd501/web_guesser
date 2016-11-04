b23e88776558b6ef389794fd73d70588dfd14a09
Jill Donohue <jillmd501@gmail.com>
2016-11-04 12:23:45 -0600

    Add  task save method

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,3 +1,5 @@
+require_relative '../models/task.rb'
+
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
@@ -6,7 +8,7 @@ class TaskManagerApp < Sinatra::Base
   end
 
   get '/tasks' do
-    @tasks = ["task1", "task2", "task3"]
+    @tasks = Task.all
     erb :index
   end
 
