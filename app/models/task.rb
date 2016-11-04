fc949d3ba5c05c5f9219a898176d48528c2ef25a
Jill Donohue <jillmd501@gmail.com>
2016-11-04 12:48:53 -0600

    Add find method

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 require 'sqlite3'
 
+attr_reader :title, :description
+
 class Task
   def initialize(task_params)
     @description = task_params["description"]
@@ -15,5 +17,12 @@ class Task
 	tasks.map do |task|
 	  Task.new(task)
 	end
+  end
+
+  def self.find(id)
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    task = database.execute("SELECT * FROM tasks WHERE id = ?", id).first
+    Task.new(task)
   end
 end
